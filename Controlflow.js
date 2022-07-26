981fff122e114b8315a5d151e50bf1b691bef045
pratiksha <pratikshapetkar25@gmail.com>
2022-07-26 08:20:33 +0530

    added iflese

---
+++ Controlflow.js
@@ -0,0 +1,24 @@
+//  if statements
+// const age = 25;
+
+// if (age<18){
+
+//     console.log('you cannot drive car')
+// }
+
+// if(age>=18){
+//     console.log('You can drive car')
+// }
+
+// executes only when its true
+
+const age = 15;
+if(age>=18){
+    console.log('You can drive car')
+}
+else{
+    console.log('you cannot drive car')
+}
+//it will execute even if one condition is true
+
+//if condtn is false else will execute
