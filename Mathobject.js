e3cb9ce8d0fc0561ab4a47f89431c865eb1ff4e4
pratiksha <pratikshapetkar25@gmail.com>
2022-07-26 07:54:16 +0530

    added methods

--- Mathobject.js
+++ Mathobject.js
@@ -1,1 +1,27 @@
+//getting value of PI
 console.log(Math.PI);
+
+
+//Math round methods
+console.log(Math.round(4.7));
+console.log(Math.round(4.2));
+
+//Math power method
+console.log(Math.pow(3,2))
+
+//Math square root method
+console.log(Math.sqrt(64));
+
+//math celing and floor method
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.7));
+
+//min method
+console.log(Math.min(9,2,6));
+
+//max method
+console.log(Math.max(100,30,80));
+
+//math random method
+console.log(Math.random());
+
