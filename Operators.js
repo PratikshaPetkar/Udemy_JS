6ccecca6dec9a7600917f605a9d1902742246edd
pratiksha <pratikshapetkar25@gmail.com>
2022-07-26 08:09:24 +0530

    added operators

---
+++ Operators.js
@@ -0,0 +1,32 @@
+//Arithmatic Operators
+// console.log(10 + 20);
+// console.log(10 - 20);
+// console.log(2 * 10);
+// console.log(50/5);
+// console.log(10 % 3);
+
+//Assignment Operators
+// let num = 5;
+// num += 5;    //num = num + 5
+// num -= 5;
+// num *= 5;
+// num /= 5;
+// num %= 5;
+
+// console.log(num);
+
+//comparision operator
+// console.log (5 == 6);
+// console.log (5 != 6);
+// console.log (10<5);
+// console.log (10>5)
+// console.log (10 <=10);
+
+console.log (5 == '5'); //checks value 
+console.log (5 === '5'); //checks value and type
+
+
+
+
+
+
